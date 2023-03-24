9fdeac2bf1f328f499e3020cd610b2c6d702ab05
Remy Skelton <124379820+Messi-Sui@users.noreply.github.com>
2023-03-24 15:04:19 +0000

    Started

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Copyright (c) 2023 Skelton All rights reserved
+//
+// Created by: Remy Skelton
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+function enterClicked () {
+  const TAX_RATE=0.2005
+  
+  // User data input
+  let aEdge = parseFloat(document.getElementById("hours-worked").value)
+  let totalPay = hoursWorked * hourlyRate;
+  let tax = totalPay * TAX_RATE;
+  let takeHomeSalary = totalPay - tax;
+  
+  // Data given back to user
+  document.getElementById('takeHomeSalary').innerHTML = 'After taxes your take home slary is $' + takeHomeSalary.toFixed(2)
+  document.getElementById('taxPaid').innerHTML = 'Amount of tax money the goverment will resive from you is $ ' + tax.toFixed(2)
+}
